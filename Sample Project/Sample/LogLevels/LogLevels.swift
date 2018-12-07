8ca4eca5890a96d307d05265e024c0603740bc90
robbroadwell <hello@robbroadwell.com>
2018-12-07 12:56:44 -0700

    Add Sample Project

---
+++ Sample Project/Sample/LogLevels/LogLevels.swift
@@ -0,0 +1,17 @@
+//
+//  LogLevels.swift
+//  Sample
+//
+//  Created by Rob Broadwell on 12/7/18.
+//  Copyright © 2018 Noah Prado. All rights reserved.
+//
+
+import Foundation
+
+struct Log {
+    
+}
+
+struct LogLevels {
+    
+}
---
+++ Sample Project/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogLevels
+//
+//  Created by Rob Broadwell on 12/7/18.
+//  Copyright © 2018 Noah Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
